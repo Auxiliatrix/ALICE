c9c064a67a99d585eb8e732685c3d49f5150f2c3
Alina Kim <Squeakadoodle6084@gmail.com>
2020-09-13 08:31:11 -0700

    Made constants file

---
+++ src/alice/configuration/calibration/Constants.java
@@ -0,0 +1,10 @@
+package alice.configuration.calibration;
+
+public class Constants {
+	public static final String COMMAND_PREFIX = "%";
+	public static final String NAME = "AL | CE";
+	public static final long[] DEVELOPER_IDS = new long[] {
+			Long.parseLong("246562987651891200"),
+			Long.parseLong("365715538166415362"),
+	};
+}
