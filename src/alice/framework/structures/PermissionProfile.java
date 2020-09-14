47bd59345ff679b7bb4a4ee0e2cb872ac75887c6
Alina Kim <Squeakadoodle6084@gmail.com>
2020-09-13 22:36:28 -0700

    Modified PermissionProfile to have builder and factory methods

--- src/alice/framework/structures/PermissionProfile.java
+++ src/alice/framework/structures/PermissionProfile.java
@@ -8,28 +8,77 @@ import reactor.core.publisher.Mono;
 
 public class PermissionProfile {
 	
+	/* Internal Verification Predicate */
 	private Predicate<Mono<Member>> verification; 
 	
+	/* Constructors */
 	public PermissionProfile() {
-		verification = null;
+		this(null);
 	}
 	
+	private PermissionProfile(Predicate<Mono<Member>> preset) {
+		verification = preset;
+	}
+	
+	/* Primary Use Function */
 	public boolean verify(Mono<Member> member) {
 		return verification.test(member);
 	}
 	
+	/* Factory Methods */
+	public static PermissionProfile getAnyonePreset() {
+		return new PermissionProfile(member -> true);
+	}
+	
+	public static PermissionProfile getAdminPreset() {
+		return new PermissionProfile(member -> hasPermission(member, Permission.ADMINISTRATOR));
+	}
+	
+	public static PermissionProfile getNotBotPreset() {
+		return new PermissionProfile(member -> !isBot(member));
+	}
+	
+	/* Builder Methods */
 	public PermissionProfile and(Permission permission) {
 		verification = verification == null 
-							? member -> { return member.block().getBasePermissions().block().contains(permission); } 
-							: member -> { return verification.test(member) && member.block().getBasePermissions().block().contains(permission); };
+							? member -> hasPermission(member, permission)
+							: member -> verification.test(member) && hasPermission(member, permission);
 		return this;
 	}
 	
 	public PermissionProfile or(Permission permission) {
 		verification = verification == null 
-							? member -> { return member.block().getBasePermissions().block().contains(permission); } 
-							: member -> { return verification.test(member) || member.block().getBasePermissions().block().contains(permission); };
+							? member -> hasPermission(member, permission)
+							: member -> verification.test(member) || hasPermission(member, permission);
 		return this;
+	}
+	
+	public PermissionProfile andNotBot() {
+		verification = verification == null
+							? member -> isBot(member)
+							: member -> verification.test(member) && isBot(member);
+		return this;
+	}
+	
+	public PermissionProfile orNotBot() {
+		verification = verification == null
+							? member -> isBot(member)
+							: member -> verification.test(member) || isBot(member);
+		return this;
+	}
+	
+	public PermissionProfile invert() {
+		verification = member -> !verification.test(member);
+		return this;
+	}
+	
+	/* Helper Functions */
+	private static boolean hasPermission( Mono<Member> member, Permission permission ) {
+		return member.block().getBasePermissions().block().contains(permission);
+	}
+	
+	private static boolean isBot( Mono<Member> member ) {
+		return member.block().isBot();
 	}
 	
 }
