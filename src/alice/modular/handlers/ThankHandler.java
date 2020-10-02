d184924f3ee1ef1cbb8b064ce5e28fa7d9b8003e
Alina Kim <Squeakadoodle6084@gmail.com>
2020-10-01 23:43:00 -0700

    Added thank handler for reputation usage

---
+++ src/alice/modular/handlers/ThankHandler.java
@@ -0,0 +1,42 @@
+package alice.modular.handlers;
+
+import alice.configuration.calibration.Constants;
+import alice.framework.actions.Action;
+import alice.framework.handlers.Handler;
+import alice.framework.main.Brain;
+import alice.framework.structures.AtomicSaveFile;
+import alice.framework.structures.PermissionProfile;
+import alice.framework.structures.TokenizedString;
+import alice.modular.actions.MessageCreateAction;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.channel.Channel.Type;
+
+public class ThankHandler extends Handler<MessageCreateEvent> {
+
+	public ThankHandler() {
+		super("Thank", false, MessageCreateEvent.class);
+	}
+
+	@Override
+	protected boolean trigger(MessageCreateEvent event) {
+		if( event.getMessage().getChannel().block().getType() != Type.GUILD_TEXT || event.getMessage().getAuthor().isEmpty()) {
+			return false;
+		}
+		AtomicSaveFile guildData = Brain.guildIndex.get(event.getGuild().block().getId().asString());
+
+		String lastRepKey = String.format("%d_lastrep", event.getMessage().getAuthorAsMember().block().getId().asLong());
+		if( !guildData.has(lastRepKey) ) {
+			guildData.put(lastRepKey, System.currentTimeMillis() - (Constants.REPUTATION_INTERVAL+1000));
+		}
+		long lastRep = guildData.getLong(lastRepKey);
+		long remaining = Constants.REPUTATION_INTERVAL - (System.currentTimeMillis() - lastRep);
+		
+		return new TokenizedString(event.getMessage().getContent()).containsAnyTokensIgnoreCase("thank", "thanks", "ty", "tyty") && !PermissionProfile.isBot(event.getMessage().getAuthor(), event.getGuild()) && remaining <= 0;
+	}
+
+	@Override
+	protected Action execute(MessageCreateEvent event) {
+		return new MessageCreateAction(event.getMessage().getChannel(), "You can thank them by typing `%rep @user`!\nYou can award one every four hours, and should have one available now.");
+	}
+
+}
