60870e10acb203428ec6de81f5e04d058366bee9
Alina Kim <Squeakadoodle6084@gmail.com>
2021-07-08 16:52:44 -0700

    Added DependentStacker with multiple dependencies

---
+++ src/alice/framework/tasks/MultipleDependentStacker.java
@@ -0,0 +1,84 @@
+package alice.framework.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * A DependentStacker implementation that allows for Tasks and Effects to depend on the results of multiple Monos.
+ * This structure allows all the Monos used in the construction of an instance of this object to be executed at once before the production of a Mono response.
+ * @author Auxiliatrix
+ *
+ */
+public class MultipleDependentStacker extends Stacker {
+
+	/**
+	 * The Monos that the stacked Monoables depend on.
+	 */
+	private Flux<Mono<?>> dependencies;
+	
+	/**
+	 * Tasks that will produce an executable response based on the results of the dependency Mono.
+	 */
+	private List<Function<List<?>, Mono<?>>> tasks;
+	
+	/**
+	 * Consumer functions that will execute based on the results of the dependency Mono.
+	 */
+	private List<Consumer<List<?>>> effects;
+		
+	/**
+	 * Constructs a DependentStacker based on a Mono whos execution is being depended on.
+	 * @param dependency Mono to depend on
+	 */
+	public MultipleDependentStacker(Mono<?>... dependencies) {
+		super();
+		this.dependencies = Flux.fromArray(dependencies);
+		this.tasks = new ArrayList<Function<List<?>, Mono<?>>>();
+		this.effects = new ArrayList<Consumer<List<?>>>();		
+	}
+	
+	/**
+	 * Stacks a Task whose executable response depends on the dependency mono.
+	 * @param task Task to stack
+	 * @return Mono<?> cumulated executable response so far
+	 */
+	public Mono<?> addTask(Function<List<?>, Mono<?>> task) {
+		tasks.add(task);
+		return toMono();
+	}
+	
+	/**
+	 * Stacks an effect which depends on the dependency Mono
+	 * @param consumer Consumer effect to stack
+	 * @return Mono<?> cumulated executable response so far
+	 */
+	public Mono<?> addEffect(Consumer<List<?>> consumer) {
+		effects.add(consumer);
+		return toMono();
+	}
+	
+	@Override
+	public Mono<?> toMono() {
+		return super.toMono().and(dependencies.map(m -> m.block()).collectList().flatMap(t -> {	// Resolves Monos in Flux, flattens Flux to Mono of List, and applies resolved List to Effects/Tasks
+			for( Object o : t ) {
+				System.out.println(o);
+			}
+			Mono<Void> process = Mono.fromRunnable(() -> {
+				for( Consumer<List<?>> effect : effects ) {
+					effect.accept(t);
+				}
+			});
+			for( Function<List<?>, Mono<?>> task : tasks ) {
+				process = process.and(task.apply(t));
+			}
+			
+			return process;
+		}));
+	}
+	
+}
