1d3e48bc7a507c8cfee79cc9c168238f4916b63e
Alina Kim <Squeakadoodle6084@gmail.com>
2022-06-16 00:39:25 -0700

    Finalized invitation tracking

--- src/alice/modular/features/InviteDeleteFeature.java
+++ src/alice/modular/features/InviteDeleteFeature.java
@@ -1,23 +1,19 @@
 package alice.modular.features;
 
 import java.util.Comparator;
-import java.util.List;
 
+import alice.framework.database.SaveFileInterface;
+import alice.framework.database.SaveSyncProxy;
 import alice.framework.features.Feature;
 import alice.framework.main.Brain;
 import alice.framework.tasks.Stacker;
 import alice.framework.utilities.FileIO;
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.InviteDeleteEvent;
-import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Member;
 import reactor.core.publisher.Mono;
 
 public class InviteDeleteFeature extends Feature<InviteDeleteEvent> {
-
-	public static long guildID = -1;
-	public static long roleID = -1;
-	public static List<String> uniqueCodes = null;
 	
 	public InviteDeleteFeature() {
 		super("invdel", InviteDeleteEvent.class);
@@ -25,41 +21,38 @@ public class InviteDeleteFeature extends Feature<InviteDeleteEvent> {
 
 	@Override
 	protected boolean listen(InviteDeleteEvent type) {
-		if( uniqueCodes == null || guildID < 1 || roleID < 1 ) {
+		SaveFileInterface sfi = SaveSyncProxy.of("lab/invite_user.csv");
+		if( sfi.has("roleID") && sfi.has("guildID") && sfi.has("invite_map") && sfi.has("self_invites")) {
+			System.out.println("New invite link used.");
+			return sfi.getLong("guildID") == type.getGuildId().get().asLong();
+		} else {
 			System.err.println("InviteTrackerFeature conditions not met.");
 			return false;
 		}
-		System.out.println("Join detected");
-		return guildID == type.getGuildId().get().asLong();
 	}
 
 	@Override
 	protected Mono<?> respond(InviteDeleteEvent type) {
 		Stacker stacker = new Stacker();
-		
-		Member target = Brain.getMembers(type.getGuildId().get()).sort(new Comparator<Member>() {
-			@Override
-			public int compare(Member o1, Member o2) {
-				return -o1.getJoinTime().get().compareTo(o2.getJoinTime().get());
-			}}).blockFirst();
-		String code = type.getCode();
-		stacker.append(target.addRole(Snowflake.of(roleID)));
-		stacker.append(() -> {
-			FileIO.appendToFile("tmp/user_associations.csv", String.format("%s,%s#%s,%s\n", code, target.getUsername(), target.getDiscriminator(), target.getId().asString()));
-			System.out.println(target.getUsername() + "#" + target.getDiscriminator());
-		});
+		SaveFileInterface sfi = SaveSyncProxy.of("lab/invite_user.csv");
+
+		if( sfi.getJSONArray("self_invites").toList().contains(type.getCode()) ) {
+			Member target = Brain.getMembers(type.getGuildId().get()).sort(new Comparator<Member>() {
+				@Override
+				public int compare(Member o1, Member o2) {
+					return -o1.getJoinTime().get().compareTo(o2.getJoinTime().get());
+				}}).blockFirst();
+			String code = type.getCode();
+			stacker.append(target.addRole(Snowflake.of(sfi.getLong("roleID"))));
+			stacker.append(() -> {
+				SaveFileInterface inviteMap = sfi.getJSONObject("invite_map");
+				inviteMap.put(target.getId().asString(), code);
+				FileIO.appendToFile("tmp/user_associations.csv", String.format("%s,%s#%s,%s\n", code, target.getUsername(), target.getDiscriminator(), target.getId().asString()));
+				System.out.println(target.getUsername() + "#" + target.getDiscriminator());
+			});
+		}
 		
 		return stacker.toMono();
-	}
-	
-	public static void trackCustomInvites(Guild guild) {
-//		List<String> invites = guild.getInvites().filter(ei -> Brain.client.getSelfId().equals(ei.getInviterId().get())).map(ei -> ei.getCode()).collectList().block();
-		List<String> invites = guild.getInvites().filter(ei -> 
-			Snowflake.of("367437754034028545").equals(ei.getInviterId().get())
-			|| Brain.client.getSelfId().equals(ei.getInviterId().get())
-			).map(ei -> ei.getCode()).collectList().block();
-		System.out.println(invites.size());
-		uniqueCodes = invites;
 	}
 
 }
--- src/alice/modular/features/InviteTrackerActivationFeature.java
+++ src/alice/modular/features/InviteTrackerActivationFeature.java
@@ -1,9 +1,16 @@
 package alice.modular.features;
 
+import java.util.List;
+
+import alice.framework.database.SaveArrayInterface;
+import alice.framework.database.SaveFileInterface;
+import alice.framework.database.SaveSyncProxy;
 import alice.framework.features.MessageFeature;
+import alice.framework.main.Brain;
 import alice.framework.structures.PermissionProfile;
 import alice.framework.structures.TokenizedString;
 import alice.framework.tasks.DependentStacker;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Guild;
 import reactor.core.publisher.Mono;
@@ -29,12 +36,38 @@ public class InviteTrackerActivationFeature extends MessageFeature {
 			if( ts.size() < 2 ) {
 				System.err.println("No role ID provided.");
 			} else {
-				InviteDeleteFeature.trackCustomInvites(g);
-				InviteDeleteFeature.guildID = g.getId().asLong();
-				InviteDeleteFeature.roleID = Long.parseLong(ts.getToken(1).getContent());
+				SaveFileInterface sfi = SaveSyncProxy.of("lab/invite_user.csv");
+				if( !sfi.has("invite_map") ) {
+					sfi.putJSONObject("invite_map");
+				}
+				if( !sfi.has("guildID") ) {
+					sfi.put("guildID", g.getId().asLong());
+				}
+				if( !sfi.has("roleID") ) {
+					sfi.put("roleID", Long.parseLong(ts.getToken(1).getContent()));
+				}
+				trackCustomInvites(g);
 			}
 		});
 		return stacker.toMono();
+	}
+	
+	public static void trackCustomInvites(Guild guild) {
+		SaveFileInterface sfi = SaveSyncProxy.of("lab/invite_user.csv");
+		if( !sfi.has("self_invites") ) {
+			sfi.putJSONArray("self_invites");
+		}
+		SaveArrayInterface sai = sfi.getJSONArray("self_invites");
+		List<String> invites = guild.getInvites().filter(ei -> 
+			Snowflake.of("367437754034028545").equals(ei.getInviterId().get())
+			|| Brain.client.getSelfId().equals(ei.getInviterId().get())
+			).map(ei -> ei.getCode()).collectList().block();
+		System.out.println(invites.size());
+		for( String invite : invites ) {
+			if( !sai.toList().contains(invite) ) {
+				sai.put(invite);
+			}
+		}
 	}
 	
 }
