be5485d73572e72eb4145d1c38082e6386295ad5
Alina Kim <Squeakadoodle6084@gmail.com>
2022-07-11 17:13:35 -0700

    Reworked modules to work with new synchronization framework

--- src/alice/modular/modules/ReputationModule.java
+++ src/alice/modular/modules/ReputationModule.java
@@ -14,6 +14,8 @@ import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
+import discord4j.rest.util.Permission;
+import discord4j.rest.util.PermissionSet;
 import reactor.core.publisher.Mono;
 
 public class ReputationModule extends MessageModule {
@@ -25,6 +27,7 @@ public class ReputationModule extends MessageModule {
 	@Override
 	public Command<MessageCreateEvent> buildCommand(Builder<MessageCreateEvent> dfb) {
 		DependencyManager<MessageCreateEvent, MessageChannel> mcdm = dfb.addDependency(mce -> mce.getMessage().getChannel());
+		DependencyManager<MessageCreateEvent, PermissionSet> psdm = dfb.addDependency(mce -> mce.getMember().get().getBasePermissions());
 		DependencyFactory<MessageCreateEvent> df = dfb.build();
 				
 		Command<MessageCreateEvent> command = new Command<MessageCreateEvent>(df);
@@ -52,27 +55,11 @@ public class ReputationModule extends MessageModule {
 						.description(String.format("This user has :scroll:%s reputation!", rep_map.get(s.asString())))
 						.color(Color.YELLOW)
 						.author(targetUser.getUsername(), null, targetUser.getAvatarUrl())
-						.build())
-					.and(Mono.fromRunnable(() -> {System.out.println("Rep");}));
+						.build());
 		});
 		
 		Command<MessageCreateEvent> arg = command.addSubcommand();
 		arg.withCondition(MessageModule.getArgumentsCondition(2));
-		
-		Command<MessageCreateEvent> setup = arg.addSubcommand();
-		setup.withCondition(mce -> {
-			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(mce.getGuildId().get().asLong());
-			return !ssf.has("%rep_map") || !ssf.has("%rep_last");
-		});
-		setup.withSideEffect(mce -> {
-			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(mce.getGuildId().get().asLong());
-			if( !ssf.has("%rep_map") ) {
-				ssf.putJSONObject("%rep_map");
-			}
-			if( !ssf.has("%rep_last") ) {
-				ssf.putJSONObject("%rep_last");
-			}
-		});
 		
 //		Command<MessageCreateEvent> leadCommand = arg.addSubcommand();
 				
@@ -80,17 +67,15 @@ public class ReputationModule extends MessageModule {
 		repCommand.withCondition(MessageModule.getMentionsCondition(1));
 		repCommand.withDependentEffect(d -> {
 			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(d.getEvent().getGuildId().get().asLong());
-//			if( ssf == null ) {
-//				System.out.println("null save file");
-//			}
-//			if( !ssf.has("%rep_map") ) {
-//				ssf.putJSONObject("%rep_map");
-//			}
-//			if( !ssf.has("%rep_last") ) {
-//				ssf.putJSONObject("%rep_last");
-//			}
+			if( !ssf.has("%rep_map") ) {
+				ssf.putJSONObject("%rep_map");
+			}
+			if( !ssf.has("%rep_last") ) {
+				ssf.putJSONObject("%rep_last");
+			}
 			SyncedJSONObject rep_map = ssf.getJSONObject("%rep_map");
 			SyncedJSONObject rep_last = ssf.getJSONObject("%rep_last");
+			boolean admin = psdm.requestFrom(d).contains(Permission.ADMINISTRATOR);
 			MessageChannel mc = mcdm.requestFrom(d);
 			Snowflake s = d.getEvent().getMessage().getAuthor().get().getId();
 			User targetUser = d.getEvent().getMessage().getUserMentions().get(0);
@@ -102,14 +87,14 @@ public class ReputationModule extends MessageModule {
 			if( !rep_map.has(s.asString()) ) {
 				rep_map.put(s.asString(), 0);
 			}
-			if( s.equals(target) ) {
+			if( s.equals(target) && !admin ) {
 				return mc.createMessage(EmbedBuilders.applyErrorFormat("You cannot give reputation to yourself!", EmbedBuilders.ERR_PERMISSION));
 			} else {
 				if( rep_last.has(s.asString()) ) {
 					long last = rep_last.getLong(s.asString());
 					long current = System.currentTimeMillis();
 					long dif = current - last;
-					if( dif < 14400000 ) {
+					if( dif < 14400000 && !admin ) {
 						if( (14400000-dif) > 60000 ) {
 							return mc.createMessage(
 									EmbedCreateSpec.builder()
@@ -134,7 +119,7 @@ public class ReputationModule extends MessageModule {
 					rep_map.increment(s.asString());
 					
 					rep_last.put(s.asString(), System.currentTimeMillis());
-				}).and(mc.createMessage(EmbedCreateSpec.builder().description(String.format("This user now has :scroll:%s reputation!", rep_map.get(target.asString()))).color(Color.YELLOW).author(targetUser.getUsername(), null, targetUser.getAvatarUrl()).build()));
+				}).then(mc.createMessage(EmbedCreateSpec.builder().description(String.format("This user now has :scroll:%s reputation!", rep_map.getInt(target.asString())+1)).color(Color.YELLOW).author(targetUser.getUsername(), null, targetUser.getAvatarUrl()).build()));
 			}
 		});
 		
---
+++ src/alice/modular/modules/TestModule.java
@@ -0,0 +1,66 @@
+package alice.modular.modules;
+
+import alice.framework.database.SyncedJSONObject;
+import alice.framework.database.SyncedSaveFile;
+import alice.framework.dependencies.Command;
+import alice.framework.dependencies.DependencyFactory;
+import alice.framework.modules.MessageModule;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Mono;
+
+public class TestModule extends MessageModule {
+
+	public TestModule() {
+		super();
+	}
+	
+	public Command<MessageCreateEvent> buildCommand(DependencyFactory.Builder<MessageCreateEvent> dfb) {
+		DependencyFactory<MessageCreateEvent> df = dfb.build();
+		
+		Command<MessageCreateEvent> command = new Command<MessageCreateEvent>(df);
+		command.withCondition(MessageModule.getInvokedCondition("%test"));
+		
+		Command<MessageCreateEvent> execOrder = command.addSubcommand();
+		command.withCondition(MessageModule.getArgumentCondition(1, "exor"));
+		
+		Command<MessageCreateEvent> c1 = execOrder.addSubcommand();
+		c1.withCondition(mce -> {
+			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(mce.getGuildId().get().asLong());
+			return !ssf.has("tester");
+		});
+		c1.withSideEffect(mce -> {
+			System.out.println("..");
+			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(mce.getGuildId().get().asLong());
+			ssf.putJSONObject("tester");
+		});
+		c1.withDependentSideEffect(d -> {
+			System.out.println("....");
+			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(d.getEvent().getGuildId().get().asLong());
+			SyncedJSONObject tester = ssf.getJSONObject("tester");
+			tester.put("test1", "......");
+		});
+		
+		Command<MessageCreateEvent> c2 = execOrder.addSubcommand();
+		c2.withEffect(mce -> {
+			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(mce.getGuildId().get().asLong());
+			SyncedJSONObject tester = ssf.getJSONObject("tester");
+			System.out.println(tester.get("test1"));
+			return Mono.fromRunnable(() -> {
+				System.out.println("........");
+				tester.put("test2", "..........");
+			});
+		});
+		c2.withDependentEffect(d -> {
+			SyncedJSONObject ssf = SyncedSaveFile.ofGuild(d.getEvent().getGuildId().get().asLong());
+			SyncedJSONObject tester = ssf.getJSONObject("tester");
+			System.out.println(tester.get("test2"));
+			return Mono.fromRunnable(() -> {
+				System.out.println("............");
+				ssf.remove("tester");
+			});
+		});
+		
+		return command;
+	}	
+	
+}
